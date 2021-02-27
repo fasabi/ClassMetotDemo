9b87be92cdf7b7046ffb9b98647a08324572b133
Fatıma Saliha Biter <f.saliha.biter@hotmail.com>
2021-02-27 13:55:46 +0300

    MyDictionary projesi eklendi. 27.01.2021

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Saliha");
+            myDictionary.Add(2, "Tuğba");
+
+            myDictionary.Print();
+
+
+            myDictionary.Add(3, "Büşra");
+
+            myDictionary.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
